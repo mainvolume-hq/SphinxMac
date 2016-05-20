8841a177cef55a11691ea4a051c9728df7e3f517
Mr S <mainvolume@mac.com>
2016-05-20 15:51:25 +0200

    Initial Commit

---
+++ SphinxMac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SphinxMac
+//
+//  Created by mainvolume on 5/20/16.
+//  Copyright © 2016 mainvolume. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SphinxMac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SphinxMac
+//
+//  Created by mainvolume on 5/20/16.
+//  Copyright © 2016 mainvolume. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
